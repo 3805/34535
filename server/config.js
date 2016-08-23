17e964d1820b65db813db517317709619ec0af6a
3805 <jiki241@gmail.com>
2016-08-24 00:33:53 +0200

    [add] cors access methods

--- server/config.js
+++ server/config.js
@@ -6,8 +6,12 @@ module.exports = function(app, service) {
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    )
+    res.header(
+      'Access-Control-Allow-Methods',
+      'POST, GET, PUT, DELETE, OPTIONS'
     )
     next()
   })
