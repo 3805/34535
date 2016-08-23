e14f6bfe86f3dfa7fd94fd2a30b4d26c71713d5b
3805 <jiki241@gmail.com>
2016-08-24 00:40:19 +0200

    [add] cors module test

--- server/config.js
+++ server/config.js
@@ -3,18 +3,20 @@ module.exports = function(app, service) {
 
   app.set('superSecret', require('./mongo').secret)
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    )
-    res.header(
-      'Access-Control-Allow-Methods',
-      'POST, GET, PUT, DELETE, OPTIONS'
-    )
-    next()
-  })
+  var cors = require('cors')
+  app.use(cors())
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*")
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept",
+  //   )
+  //   res.header(
+  //     "Access-Control-Allow-Methods",
+  //     "POST, GET, PUT, DELETE, OPTIONS"
+  //   )
+  //   next()
+  // })
 
   service.mongoose.Promise = Promise
   service.mongoose.connect(require('./mongo').dbUrl())
